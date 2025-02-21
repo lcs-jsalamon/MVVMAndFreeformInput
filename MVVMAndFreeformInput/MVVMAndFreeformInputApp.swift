903c89f1848f223d886211f7eaad381e4942ac76
Josephine Salamon <jsalamon@lcs.on.ca>
2025-02-21 11:25:33 -0500

    Initial Commit

---
+++ MVVMAndFreeformInput/MVVMAndFreeformInputApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMAndFreeformInputApp.swift
+//  MVVMAndFreeformInput
+//
+//  Created by Josephine Salamon on 2025-02-21.
+//
+
+import SwiftUI
+
+@main
+struct MVVMAndFreeformInputApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MVVMAndFreeformInputTests/MVVMAndFreeformInputTests.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMAndFreeformInputTests.swift
+//  MVVMAndFreeformInputTests
+//
+//  Created by Josephine Salamon on 2025-02-21.
+//
+
+import Testing
+@testable import MVVMAndFreeformInput
+
+struct MVVMAndFreeformInputTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
